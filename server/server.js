997752efe55493a58fd51014e1b132b6d8137bc3
unknown <julie.kim520@gmail.com>
2020-03-04 22:57:31 +1100

    section3 Node API- mongoDB connect

--- server/server.js
+++ server/server.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
+
+// connect to db
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology:true,
+  useCreateIndex: true
+})
+.then(() => console.log('DB connected'))
+.catch(err => console.log('DB CONNECTION ERROR: ', err));
 
 // routing
 // import routes
@@ -12,12 +23,16 @@ const authRoutes = require("./routes/auth");
 
 // app middlewares
 app.use(morgan("dev"));
-app.use(cors()); // allows all origins
+app.use(bodyParser.json());
+// app.use(cors()); // allows all origins
+if ((process.env.NODE_ENV = "development")) {
+  app.use(cors({ origin: `http://localhost:3000` }));
+}
 
 // middleware : use method
 app.use("/api", authRoutes);
 
-const port = process.env.port || 8000;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`API is running on port ${port}`);
 });
